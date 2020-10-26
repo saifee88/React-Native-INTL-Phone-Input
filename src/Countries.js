cebfe08f72fc498751b265f5b4c8403a1d945cef
saifee88 <abdulqadir.saifee@retailo.co>
2020-10-26 14:11:17 +0500

    fix: appearance-order

--- src/Countries.js
+++ src/Countries.js
@@ -1,7 +1,7 @@
 const Countries = [
   { ru:"Пакистан",lt:"Pakistanas",tr:"Pakistan", en: 'Pakistan',flag: '🇵🇰',code: 'PK', dialCode: '+92', mask: '999 9999999',masking: '301 2345678' },
-  { ru:"Объединенные Арабские Эмираты",lt:"Jungtiniai Arabų Emyratai",tr:"Birleşik Arap Emirlikleri", en: 'United Arab Emirates',flag: '🇦🇪',code: 'AE', dialCode: '+971', mask: '999 999 9999', masking: "050 234 56789" },
-  { ru:"Саудовская Аравия",lt:"Saudo Arabija",tr:"Suudi Arabistan", en: 'Saudi Arabia',flag: '🇸🇦',code: 'SA', dialCode: '+966', mask: '99 999 9999',masking:'51 234 5678' }
+  { ru:"Саудовская Аравия",lt:"Saudo Arabija",tr:"Suudi Arabistan", en: 'Saudi Arabia',flag: '🇸🇦',code: 'SA', dialCode: '+966', mask: '99 999 9999',masking:'51 234 5678' },
+  { ru:"Объединенные Арабские Эмираты",lt:"Jungtiniai Arabų Emyratai",tr:"Birleşik Arap Emirlikleri", en: 'United Arab Emirates',flag: '🇦🇪',code: 'AE', dialCode: '+971', mask: '999 999 9999', masking: "050 234 56789" }
 ];
 
 export default Countries;
